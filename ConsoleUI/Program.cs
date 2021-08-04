44f9d75f0ef56e8f3ca72756b9bec8af64bb7481
Sarperbg <sarpergulfirat@hotmail.com>
2021-08-04 12:27:17 +0300

    day9

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
@@ -8,12 +9,21 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine("Araba Numarası : " + car.Id + " - " + "Araç Fiyatı :" + car.DailyPrice + "TL");
+            CarManager carManager = new CarManager(new EfCarDal());
+            Console.WriteLine("Markaya göre");
+            var carsByBrand = carManager.GetCarsByBrandId(1);
 
+            foreach (var car in carsByBrand)
+            {
+                Console.WriteLine(car.Id + " " + car.Description);
             }
+            Console.WriteLine("Renge göre");
+            var carsByColorId = carManager.GetCarsByColorId(1);
+            foreach (var car in carsByColorId)
+            {
+                Console.WriteLine(car.Id + " " + car.Description);
+            }
+
         }
     }
 }
